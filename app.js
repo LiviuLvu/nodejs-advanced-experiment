86c5fe309ee26516b68850ae82dc9bca38dd1f0b
LiviuLvu <liviuiancu@protonmail.com>
2019-04-13 10:30:01 +0300

    Add routes, serve static files, add styles

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
+// Native
 const http = require("http");
-
+const path = require("path");
+// Installed
 const express = require("express");
+const bodyParser = require("body-parser");
+// Created
+const rootDir = require("./util/path");
 
 const app = express();
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(rootDir, "public")));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
 app.use((req, res, next) => {
-  console.log`inside middleware`;
+  res.status(404).sendFile(path.join(rootDir, "views", "page404.html"));
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
